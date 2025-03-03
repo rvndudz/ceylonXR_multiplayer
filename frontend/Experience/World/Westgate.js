c90555848dbf407f41ed97a9db8f5b123335fd6b
rvndudz <rvndudz77@gmail.com>
2025-03-03 20:18:22 +0530

    Revert "Add Gaussian Splat loading functionality to Westgate.js"
    This reverts commit f436ae412183de4468e38e0f335c1a4e8aabee82.

--- frontend/Experience/World/Westgate.js
+++ frontend/Experience/World/Westgate.js
@@ -1,8 +1,5 @@
-// Westgate.js
 import Experience from "../Experience.js";
 import * as THREE from "three";
-// Import what you need from GaussianSplats3D
-import * as GaussianSplats3D from "../gaussian-splats-3d.js";
 
 export default class Westgate {
     constructor() {
@@ -14,30 +11,14 @@ export default class Westgate {
         this.setWorld();
     }
 
-    async setWorld() {
-        // ----- Existing code -----
+    setWorld() {
         this.plane = this.resources.items.plane.scene;
+
         this.collider = this.resources.items.collider.scene;
         this.octree.fromGraphNode(this.collider);
+
+
         this.scene.add(this.plane);
 
-        // ----- New code: Load and add Gaussian Splat mesh -----
-        // Create a SplatLoader instance
-        const viewer = new GaussianSplats3D.Viewer({
-            'cameraUp': [0, -1, -0.6],
-            'initialCameraPosition': [-1, -4, 6],
-            'initialCameraLookAt': [0, 4, 0],
-            'sphericalHarmonicsDegree': 0
-        });
-        viewer.addSplatScene('./Splat/converted_file.ksplat', {
-            'splatAlphaRemovalThreshold': 5,
-            'showLoadingUI': true,
-            'position': [0, 1, 0],
-            'rotation': [0, 0, 0, 1],
-            'scale': [1.5, 1.5, 1.5]
-        })
-        .then(() => {
-            viewer.start();
-        });
     }
 }
